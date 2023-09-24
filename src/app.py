0193e516018c74f60ce325864c24ad376e4c2c79
Stefano <tefanoboschetti@gmail.com>
2023-09-24 08:46:05 +0000

    ✔ [GET] user, user/id, user/id/favorites + models

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -32,18 +32,51 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route("/")# Aquí definimos el primer path de la API: GET /
+def hello():
+    return "Hello World!"  # Aquí flask devolverá "Hello World, esto podría ser un string HTML o un string JSON.
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route("/users", methods=["POST"])
+def post_message():
+    body = request.get_json()
 
-    return jsonify(response_body), 200
+    username = body.get("username", None)
+    email = body.get("email", None)
+    password = body.get("password", None)
+
+    new_user = User( username=username, email=email, password=password )
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user.serialize(), 200
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    all_users = User.query.all()
+    return [user.serialize() for user in all_users]
+
+@app.route("/users/<int:user_id>", methods=["GET"])
+def get_user_id(user_id):
+    try:
+        selected_user = User.query.get(user_id) or None
+        if selected_user == None:
+            return "jaja error"
+        else:
+            return selected_user.serialize()
+    
+    except ValueError as err:
+        return {"message": "failed to retrieve user " + err}, 500
+
+@app.route("/users/<int:user_id>/favorites", methods=["GET"])
+def get_favorites(user_id):
+    try:
+        user_favorites = Favorite.query.filter(Favorite.user_id == user_id)
+        
+        return [favorite.serialize() for favorite in user_favorites]
+        
+    except ValueError as err:
+        return {"message": "failed to retrieve user " + err}, 500
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
